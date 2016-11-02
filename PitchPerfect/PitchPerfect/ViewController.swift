6da7e52d1abd84446dc6ac5f7156afe75556af1c
Damo de Lemos <lemoss@amazon.com>
2016-11-01 20:37:44 -0600

    Improvements in UI.

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var startRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +26,18 @@ class ViewController: UIViewController {
     
     @IBAction func racordAudio(_ sender: Any) {
         recordingLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        startRecordingButton.isEnabled = false
     }
 
     @IBAction func stopRecording(_ sender: Any) {
-        print("stop recording")
+        recordingLabel.text = "Tap to record"
+        stopRecordingButton.isEnabled = false
+        startRecordingButton.isEnabled = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false
     }
 
 }
